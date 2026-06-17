e35e9a9fd0a2e24eab2b29fd562368012c302eca
Daniel Escobar <daanielescobar@gmail.com>
2026-06-17 15:14:24 -0600

    setup shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/auth-context";
 import { ClientProvider } from "./contexts/client-context";
+import { Button } from "./components/ui/button";
+import { Calendar } from "./components/ui/calendar";
 
 function App() {
   return (
@@ -8,7 +10,12 @@ function App() {
       <AuthProvider>
         <ClientProvider>
           <Routes>
-            <Route path="/" element={<div>ContaIA</div>} />
+            <Route path="/" element={
+              <>
+                <Button variant={"outline"} type="reset">ContaIA</Button>
+                <Calendar />
+              </>
+            } />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </ClientProvider>
